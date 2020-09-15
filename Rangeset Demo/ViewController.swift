1d3799917a9de8fccb2031a6879391517d629403
jonysingla <jony.singla19@gmail.com>
2020-09-15 11:51:37 +0530

    First commit

---
+++ Rangeset Demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Rangeset Demo
+//
+//  Created by Jony on 10/08/20.
+//  Copyright © 2020 Jony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var number = Array(arrayLiteral: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+      
+    }
+
+
+}
+
